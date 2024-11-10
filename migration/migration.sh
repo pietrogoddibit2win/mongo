1387c5bf731d7506a0e8018b0d9a616595768493
Pietro Goddi <pietro.goddi@bit2win.com>
2024-11-10 17:40:03 +0100

    backup migration script

--- migration/migration.sh
+++ migration/migration.sh
@@ -4,22 +4,23 @@
 source ./.env
 
 # 1. dump db from source
-mongodump "mongodb+srv://$SOURCE_MONGO_CLUSTER"
-\ --username=$SOURCE_USERNAME
-\  --password=$SOURCE_PASSWORD
-\  --db=$SOURCE_DB
-\  --out=./dump
-\  --excludeCollection=sessions
-\  --excludeCollection=accounts
-\  --excludeCollection=orders
-\  --excludeCollection=orderitems
-\  --excludeCollection=assetitems
-\  --excludeCollection=cards_13e5a329-52e8-45bd-9887-537753adbeee
-\  --excludeCollection=loyalty_members
-\  --excludeCollection=loyalty_transactions
-\  --excludeCollection=events_history
-\  --excludeCollection=flowsobjectsnumbers
-\  --excludeCollection=sequences
+mongodump "mongodb+srv://$SOURCE_MONGO_CLUSTER" \
+  --username=$SOURCE_USERNAME \
+  --password=$SOURCE_PASSWORD \
+  --db=$SOURCE_DB \
+  --out=./dump \
+  --excludeCollection=sessions
+# \
+# --excludeCollection=accounts \
+# --excludeCollection=orders \
+# --excludeCollection=orderitems \
+# --excludeCollection=assetitems \
+# --excludeCollection=cards_13e5a329-52e8-45bd-9887-537753adbeee \
+# --excludeCollection=loyalty_members \
+# --excludeCollection=loyalty_transactions \
+# --excludeCollection=events_history \
+# --excludeCollection=flowsobjectsnumbers \
+# --excludeCollection=sequences
 
 # 2. restore db to target
 mongorestore "mongodb+srv://$TARGET_MONGO_CLUSTER" \
